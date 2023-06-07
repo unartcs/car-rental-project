eeb66a07142a73677540afd5fba8c87e6556b824
unartcs <unartcs@gmail.com>
2023-06-07 15:04:44 +0200

    added Tina to test CMS capabilities

---
+++ tina/config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  token: "3a5cf42e6e75d5f882857036c9c036207395e3b4", // Get this from tina.io
+  clientId: "024e5ed9-0498-4570-89f8-bac59d8213a3", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
